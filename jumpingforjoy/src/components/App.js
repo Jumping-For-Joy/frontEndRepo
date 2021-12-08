1489d4a4c47eae8626f2005911c4892d6046da1e
benebess <b3n3b3ss@gmail.com>
2021-12-08 10:26:37 +1000

    update nav file & app.js with components

--- jumpingforjoy/src/components/App.js
+++ jumpingforjoy/src/components/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Nav from './Nav';
+import Home from './Home';
+import About from './About';
+import AllCastles from './AllCastles';
+import Castle from './Castle';
+import Enquiry from './Enquiry';
+import AllEnquiries from './AllEnquiries';
+import Enquiryform from './Enquiryform';
+import Admindashboard from './AdminDashboard';
+import AddCastle from './AddCastle';
+import EditCastle from './EditCastle';
+import Footer from './Footer';
 
 function App() {
   return (
     <>
       
       <Nav />
+      <Home />
+      <About />
+      <AllCastles />
+      <Castle />
+      <Enquiry />
+      <AllEnquiries />
+      <Enquiryform />
+      <Admindashboard />
+      <AddCastle />
+      <EditCastle />
+      <Footer />
+
 
     </>
   );
