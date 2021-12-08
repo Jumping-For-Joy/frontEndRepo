997d42d880f42d4d98e40a1d52a73f5450f80149
benebess <b3n3b3ss@gmail.com>
2021-12-08 10:08:06 +1000

    working on nav component

--- jumpingforjoy/src/components/Nav.js
+++ jumpingforjoy/src/components/Nav.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Nav =() => {
     return (
-        <div>
-            
+        <div class="navarea">
+            <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a class="active" href="#forhire">For Hire</a>
+                <a class="active" href="#about">About</a>
+            </div>
         </div>
     );
 }
