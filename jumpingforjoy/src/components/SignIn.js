a3df6a0c6455ddccebbe592969b163231b249f87
Adriana Heesh <adrianaheesh@gmail.com>
2021-12-14 11:40:18 +1000

    added state and event handlers to sign in component to handle log ins

--- jumpingforjoy/src/components/SignIn.js
+++ jumpingforjoy/src/components/SignIn.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SignIn = () => {
+    const [ userDetails, setUserDetails ] = useState({
+        email: "",
+        password: ""
+    })
+
+    function formHandler(event) {
+        setUserDetails({ ...userDetails, [event.target.name]: event.target.value})
+        console.log(userDetails)
+    }
+
+    function formSubmit(event) {
+        event.preventDefault()
+        // make our call to the database here
+    }
 
     return(
         <div>
-            <h1>Sign in here</h1>
-            <form>
+            <h1>Sign in</h1>
+            <form onSubmit={formSubmit}>
                 <label>Email:
-                    <input type="email" name="email" />
+                    <input 
+                        type="email" 
+                        name="email"
+                        value={userDetails.email}
+                        onChange={formHandler} />
                 </label>
                 <label>Password:
-                    <input type="password" name="password" />
+                    <input 
+                        type="password" 
+                        name="password"
+                        value={userDetails.password}
+                        onChange={formHandler} />
                 </label>
-                <input type="submit" value="Log in"/>
+                <button type="submit">Sign in</button>
             </form>
         </div>
     )
