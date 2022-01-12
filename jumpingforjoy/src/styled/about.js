74d8bd4294afe03e6dcfae7d3595547d5bbdcaf3
benebess <b3n3b3ss@gmail.com>
2022-01-12 17:42:45 +1000

    responsive about page

--- jumpingforjoy/src/styled/about.js
+++ jumpingforjoy/src/styled/about.js
@@ -4,13 +4,12 @@ export const Div = styled.div`
     div.about-section {
         box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
         transition: 0.3s;
-        padding-top: 30px;
-        margin-left: 600px;
-        margin-right: 400px;
-        margin-top: 60px;
-        margin-bottom: 50px;
-        width: 500px;
+        margin-top: 100px;
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
         align-items: center;
+        width: 70%;
         background: #F8FEFE;
     }
 
@@ -29,8 +28,28 @@ export const Div = styled.div`
 
     img {
         display: block;
+        padding-top: 20px;
         margin-left: auto;
         margin-right: auto;
         width: 90%;
+    }
+
+    // tablet
+    @media(min-width: 768px) {
+
+    }
+
+    // desktop
+    @media(min-width: 1200px) {
+
+    div.about-section {
+        padding-top: 30px;
+        margin-left: 600px;
+        margin-right: 400px;
+        margin-top: 60px;
+        margin-bottom: 50px;
+        width: 500px;
+        align-items: center;
+    }
     }
 `
