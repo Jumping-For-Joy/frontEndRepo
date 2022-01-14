3553a56d4dc2789ff597c0f404619f0ad9774a3b
Adriana Heesh <adrianaheesh@gmail.com>
2022-01-14 13:18:03 +1000

    configured testing files

--- jumpingforjoy/src/testing/SignIn.test.js
+++ jumpingforjoy/src/testing/SignIn.test.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { shallow, mount, render } from "enzyme";
-import "../../enzymeConfig";
+import "@testing-library/jest-dom";
 import SignIn from "../components/SignIn";
+import { StateContext } from "../utils/stateContext";
+import { render, screen, fireEvent } from "@testing-library/react";
 
-describe("test signin form", () => {
-    let wrapper; 
-
-    it("Should check the presence of an email ", () => {
-        wrapper = mount(<SignIn />);
-        console.log(wrapper)
-        wrapper.find('input[type="text"]').simulate("change"), {
-            target: { id: "email", value: "test@text.com" }
-        }
-    });
-    expect(wrapper.prop("email")).toEqual("test@text.com");
-});
+describe('SignIn', () => {
+    it('should be able to type an field', () => {
+        render(
+            <StateContext.Provider value={{store: {}, dispatch: {}}}>
+                <SignIn />
+            </StateContext.Provider>
+        );
+        // Check that the user can see the email label (i.e. component has rendered)
+        expect(screen.getByText('Email:')).toBeInTheDocument();
+        // Update the value. I'm using data-testid here but it's usually better practise
+        // to try and find elements by their label (i.e. how the customer will see it)
+        // However, to make writing the tests easier while you're still learning, using data-testid is
+        // is a lot simpler and perfectly fine
+        fireEvent.change(
+            screen.getByTestId('email'),
+            { target: { value: 'test@text.com' } }
+        );
+        // Confirm that value has updated
+        expect(screen.getByTestId('email')).toHaveValue('test@text.com')
+    })
+})
