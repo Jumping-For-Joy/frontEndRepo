1d40f9a18a8cc8b2454e8b984c3d97a64840d908
benebess <b3n3b3ss@gmail.com>
2022-01-16 16:50:15 +1000

    tidying up bookingrequest form use of shared forms

--- jumpingforjoy/src/components/BookingRequest.js
+++ jumpingforjoy/src/components/BookingRequest.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import EnquiryForm from './EnquiryForm'
 import CustomerForm from './CustomerForm'
 import {useParams} from 'react-router-dom'
-import { Div } from '../styled/bookingrequest'
+import { StyledForm } from '../styled/shared/forms'
 
 // Directed to here when a customer selects 'make an enquiry' from castle listing 
 const BookingRequest = () => {
@@ -12,13 +12,13 @@ const BookingRequest = () => {
     const {id} = useParams()
 
     return (
-        <div>
+        <StyledForm>
         {/* customer id is only true once the customer form has been successfully posted, so once it is not null, the enquiry form will render */}
             {customerId ? 
                 <EnquiryForm customerId={customerId} castleId={id} /> : 
                 <CustomerForm setCustomerId={setCustomerId} /> 
             }
-        </div>
+        </StyledForm>
     )
 }
 
